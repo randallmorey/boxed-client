65b1016466a3f39802615d7cb5f167e25d87705b
randallmorey <randall@randallmorey.com>
2015-04-29 09:26:45 -0700

    added jsqrcode package

--- Brocfile.js
+++ Brocfile.js
@@ -28,5 +28,23 @@ app.import('bower_components/i18next/i18next.js');
 // moment
 app.import('bower_components/moment/moment.js');
 app.import('bower_components/moment/locale/pt-br.js');
+// jsqrcode
+app.import('bower_components/jsqrcode/src/grid.js');
+app.import('bower_components/jsqrcode/src/version.js');
+app.import('bower_components/jsqrcode/src/detector.js');
+app.import('bower_components/jsqrcode/src/formatinf.js');
+app.import('bower_components/jsqrcode/src/errorlevel.js');
+app.import('bower_components/jsqrcode/src/bitmat.js');
+app.import('bower_components/jsqrcode/src/datablock.js');
+app.import('bower_components/jsqrcode/src/bmparser.js');
+app.import('bower_components/jsqrcode/src/datamask.js');
+app.import('bower_components/jsqrcode/src/rsdecoder.js');
+app.import('bower_components/jsqrcode/src/gf256poly.js');
+app.import('bower_components/jsqrcode/src/gf256.js');
+app.import('bower_components/jsqrcode/src/decoder.js');
+app.import('bower_components/jsqrcode/src/qrcode.js');
+app.import('bower_components/jsqrcode/src/findpat.js');
+app.import('bower_components/jsqrcode/src/alignpat.js');
+app.import('bower_components/jsqrcode/src/databr.js');
 
 module.exports = app.toTree();
